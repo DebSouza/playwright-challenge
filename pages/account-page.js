8c64dfac8ec21c293fb5241b90f9bb958b50febc
Debora Souza <debora.souza@liferay.com>
2025-02-14 05:18:10 -0300

    TC-11 Implements the AccountPage class with the selectors

---
+++ pages/account-page.js
@@ -0,0 +1,9 @@
+
+class AccountPage {
+    constructor(page) {
+        this.page = page;
+        this.successfulMessage = page.locator('.alert-success');
+    }
+}
+
+module.exports = { AccountPage };
