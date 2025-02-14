c98bbb9c3ca338f43c95f2ede429cbcdbc338f08
Debora Souza <debora.souza@liferay.com>
2025-02-14 18:48:40 -0300

    TC-16 Add test Checkout

--- tests/cart.spec.js
+++ tests/cart.spec.js
@@ -1,10 +1,18 @@
 import { test, expect, beforeEach } from "@playwright/test";
 import { LoginPage } from "../pages/login-page";
 import { Utils } from "../pages/utils";
+import { CartSummaryPage } from "../pages/cart-summary-page";
+import { AddressPage } from "../pages/address-page";
+import { ShippingPage } from "../pages/shipping-page";
+import { PaymentPage } from "../pages/payment-page";
 
-let loginPage, utils;
+let loginPage, utils, cartSummaryPage, addressPage, shippingPage, paymentPage;
 
 beforeEach(async ({ page }) => {
+  paymentPage = new PaymentPage(page);
+  shippingPage = new ShippingPage(page);
+  addressPage = new AddressPage(page);
+  cartSummaryPage = new CartSummaryPage(page);
   utils = new Utils(page);
   loginPage = new LoginPage(page, utils);
   await loginPage.navigateLoginPage();
@@ -44,4 +52,36 @@ test("Remove item to cart", async ({ page }) => {
   let finalQuantity = await utils.getCartQuantity();
 
   expect(parseInt(finalQuantity)).toBe(parseInt(quantityAfterAdd) - 1);
+});
+
+test("Checkout", async ({ page }) => {
+  await utils.searchProduct("Blouse");
+
+  await utils.clickMoreButton();
+
+  await utils.selectColor("White");
+
+  await utils.addToCartButton.click();
+
+  await expect(utils.productAddedMessage).toContainText(
+    "Product successfully added to your shopping cart",
+  );
+
+  await utils.proceedToCheckoutButton.click();
+
+  await cartSummaryPage.proceedToCheckoutButton.click();
+
+  await addressPage.proceedCheckoutButton.click();
+
+  await shippingPage.agreeCheckbox.click();
+
+  await shippingPage.proceedToCheckoutButton.click();
+
+  await paymentPage.selectPaymentMethod("cheque");
+
+  await paymentPage.confirmOrderButton.click();
+
+  await expect(paymentPage.successMessage).toContainText(
+    "Your order on My Shop is complete.",
+  );
 });
