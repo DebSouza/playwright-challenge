3542fd7830a6054ca1c89a8f7f2903a4c8b33c71
Debora Souza <debora.souza@liferay.com>
2025-02-14 05:00:06 -0300

    TC-11 Sorting the tests in alphabetical order

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -8,23 +8,33 @@ beforeEach(async ({ page }) => {
     await loginPage.navigateLoginPage();
   });
 
+  test('Login error with invalid credentials', async ({ page }) => {
+    await loginPage.signInEmailField.fill('invalid_email@test.com');
+  
+    await loginPage.signInPasswdField.fill('invalid_password');
+  
+    await loginPage.signInButton.click();
+  
+    await expect(loginPage.errorMessage).toBeVisible();
+  
+    await expect(loginPage.errorMessage).toContainText('Authentication failed.');
+  });
+
   test('Login error without email', async ({ page }) => {
     await loginPage.signInButton.click();
+  
     await expect(loginPage.errorMessage).toBeVisible();
+  
     await expect(loginPage.errorMessage).toContainText('An email address required.');
   });
   
   test('Login error without password', async ({ page }) => {
     await loginPage.signInEmailField.fill('invalid_email@test.com');
+  
     await loginPage.signInButton.click();
+  
     await expect(loginPage.errorMessage).toBeVisible();
+  
     await expect(loginPage.errorMessage).toContainText('Password is required.');
   });
   
-  test('Login error with invalid credentials', async ({ page }) => {
-    await loginPage.signInEmailField.fill('invalid_email@test.com');
-    await loginPage.signInPasswdField.fill('invalid_password');
-    await loginPage.signInButton.click();
-    await expect(loginPage.errorMessage).toBeVisible();
-    await expect(loginPage.errorMessage).toContainText('Authentication failed.');
-  });
