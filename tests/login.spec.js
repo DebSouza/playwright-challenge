412034983a83d8603fc4435c98bc3f1be17059cc
Debora Souza <debora.souza@liferay.com>
2025-02-14 05:45:17 -0300

    TC-12 Fix indentation

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,4 +1,4 @@
-import {test, expect, beforeEach} from "@playwright/test";
+import { test, expect, beforeEach } from "@playwright/test";
 import { LoginPage } from "../pages/login-page";
 import { Utils } from "../pages/utils";
 import { AccountCreationPage } from "../pages/account-creation-page";
@@ -7,61 +7,64 @@ import { AccountPage } from "../pages/account-page";
 let accountPage, accountCreationPage, loginPage, utils;
 
 beforeEach(async ({ page }) => {
-    accountPage = new AccountPage(page);
-    accountCreationPage = new AccountCreationPage(page);
-    loginPage = new LoginPage(page);
-    utils = new Utils(page);
+  accountPage = new AccountPage(page);
+  accountCreationPage = new AccountCreationPage(page);
+  loginPage = new LoginPage(page);
+  utils = new Utils(page);
 
-    await loginPage.navigateLoginPage();
-  });
+  await loginPage.navigateLoginPage();
+});
 
-  test('Create an account', async ({ page }) => {
-    const userData = await utils.generateRandomUserData();
+test("Create an account", async ({ page }) => {
+  const userData = await utils.generateRandomUserData();
 
-    await loginPage.createAccountEmailField.fill(userData.email);
-    
-    await loginPage.createAccountButton.click();
-    
-    await accountCreationPage.firstNameField.fill(userData.firstName);
-    
-    await accountCreationPage.lastNameField.fill(userData.lastName);
-    
-    await accountCreationPage.passwdField.fill(userData.password);
+  await loginPage.createAccountEmailField.fill(userData.email);
 
-    await accountCreationPage.registerButton.click();
+  await loginPage.createAccountButton.click();
 
-    await expect(accountPage.successfulMessage).toBeVisible();
-    
-    await expect(accountPage.successfulMessage).toContainText('Your account has been created.');
-  });
+  await accountCreationPage.firstNameField.fill(userData.firstName);
 
-  test('Login error with invalid credentials', async ({ page }) => {
-    await loginPage.signInEmailField.fill('invalid_email@test.com');
-  
-    await loginPage.signInPasswdField.fill('invalid_password');
-  
-    await loginPage.signInButton.click();
-  
-    await expect(loginPage.errorMessage).toBeVisible();
-  
-    await expect(loginPage.errorMessage).toContainText('Authentication failed.');
-  });
+  await accountCreationPage.lastNameField.fill(userData.lastName);
 
-  test('Login error without email', async ({ page }) => {
-    await loginPage.signInButton.click();
-  
-    await expect(loginPage.errorMessage).toBeVisible();
-  
-    await expect(loginPage.errorMessage).toContainText('An email address required.');
-  });
-  
-  test('Login error without password', async ({ page }) => {
-    await loginPage.signInEmailField.fill('invalid_email@test.com');
-  
-    await loginPage.signInButton.click();
-  
-    await expect(loginPage.errorMessage).toBeVisible();
-  
-    await expect(loginPage.errorMessage).toContainText('Password is required.');
-  });
-  
+  await accountCreationPage.passwdField.fill(userData.password);
+
+  await accountCreationPage.registerButton.click();
+
+  await expect(accountPage.successfulMessage).toBeVisible();
+
+  await expect(accountPage.successfulMessage).toContainText(
+    "Your account has been created.",
+  );
+});
+
+test("Login error with invalid credentials", async ({ page }) => {
+  await loginPage.signInEmailField.fill("invalid_email@test.com");
+
+  await loginPage.signInPasswdField.fill("invalid_password");
+
+  await loginPage.signInButton.click();
+
+  await expect(loginPage.errorMessage).toBeVisible();
+
+  await expect(loginPage.errorMessage).toContainText("Authentication failed.");
+});
+
+test("Login error without email", async ({ page }) => {
+  await loginPage.signInButton.click();
+
+  await expect(loginPage.errorMessage).toBeVisible();
+
+  await expect(loginPage.errorMessage).toContainText(
+    "An email address required.",
+  );
+});
+
+test("Login error without password", async ({ page }) => {
+  await loginPage.signInEmailField.fill("invalid_email@test.com");
+
+  await loginPage.signInButton.click();
+
+  await expect(loginPage.errorMessage).toBeVisible();
+
+  await expect(loginPage.errorMessage).toContainText("Password is required.");
+});
